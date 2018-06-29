06b698257d5c4ddac5e818fd0d59a8c406b2c6fc
user1 <yyangel05@gmail.com>
2018-06-29 13:39:37 +0900

    180629 SpringView 예제002 내용

---
+++ WebContent/WEB-INF/viewjsp/registMember.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<title>회원 등록</title>
+</head>
+<body>
+
+회원 등록 : ${memberInfo.name}, ${memberInfo.address.address1}, ${memberInfo.address.address2}
+
+</body>
+</html>
---
+++ src/controller/MemberInfoValidator.java
@@ -0,0 +1,27 @@
+package controller;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class MemberInfoValidator implements Validator {
+	
+	@Override
+	public void validate(Object target, Errors errors) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userId", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address.address1", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address.address2", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "jobCode", "required");
+		MemberInfo memberInfo = (MemberInfo) target;
+		if(memberInfo.getFavorites() == null || memberInfo.getFavorites().length == 0) {
+			errors.rejectValue("favorites", "must_select");
+		}
+	}
+	
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return MemberInfo.class.isAssignableFrom(clazz);
+	}
+
+}
